9299fcbc75d9c3e40467d99f5f02c5dd88d23de4
susmita pal <sathipal5201@gmail.com>
2026-07-10 12:00:32 +0530

    email code correction

--- backend/main.py
+++ backend/main.py
@@ -42,18 +42,18 @@ class emailrequest(BaseModel):
     subject: str
     body: str
     
-    @app.post("/scan_email")
-    def scan_email(emrequest: emailrequest):
-        return{
+@app.post("/scan_email")
+def scan_email(emrequest: emailrequest):
+    return{
             "sender": emrequest.sender,
             "subject": emrequest.subject,
             "body": emrequest.body,
             "risk_score": 85,
             "status": "Potential Phishing",
             "reason": [
-                "Suspicious sender domain",
-                "Urgent language in subject",
-                "Contains links to unknown sites"
+            "Suspicious sender domain",
+            "Urgent language in subject",
+            "Contains links to unknown sites"
             ]
         }
-        
+
