d4875dde998d8e2075335d62b971f5495c91ade5
susmita pal <sathipal5201@gmail.com>
2026-07-10 11:48:23 +0530

    added email request

--- backend/main.py
+++ backend/main.py
@@ -37,3 +37,23 @@ def analyze(request: URLrequest):
         ]
     }
     
+class emailrequest(BaseModel):
+    sender: str
+    subject: str
+    body: str
+    
+    @app.post("/scan_email")
+    def scan_email(emrequest: emailrequest):
+        return{
+            "sender": emrequest.sender,
+            "subject": emrequest.subject,
+            "body": emrequest.body,
+            "risk_score": 85,
+            "status": "Potential Phishing",
+            "reason": [
+                "Suspicious sender domain",
+                "Urgent language in subject",
+                "Contains links to unknown sites"
+            ]
+        }
+        
